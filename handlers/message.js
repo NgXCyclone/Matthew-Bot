b6246be045fc80632ddae3d7286aa30d8c2f1844
NgXCyclone <44649334+NgXCyclone@users.noreply.github.com>
2021-01-28 01:56:18 +0000

    Create message.js

---
+++ handlers/message.js
@@ -0,0 +1,30 @@
+const {bot} = require('../index.js');
+const config = require("../config.json")
+
+bot.on("message", async message => {
+  if (message.author.bot) return;
+  if (message.channel.type != "text") return;
+
+		let prefix = `<@${bot.user.id}>`
+    let args = message.content.slice(prefix.length).trim().split(' ');
+	  let cmd = args.shift().toLowerCase();
+ 	  let command;
+  
+ 		const regex = new RegExp(`^<@!?${bot.user.id}>( |)$`);
+ 		if (message.content.match(regex)) {
+	message.channel.send(`My prefix is ${prefix} you fucking twat, also use ${prefix}help`).catch(err => console.log(err));
+	}
+  
+  if (!message.content.startsWith(prefix)) return;
+  
+  if (bot.commands.has(cmd)) {
+    command = bot.commands.get(cmd);
+   } else {
+    command = bot.commands.get(bot.aliases.get(cmd));
+   }
+  
+  if (command) command.run(bot, message, args).catch(console.error);
+	
+
+
+	});
