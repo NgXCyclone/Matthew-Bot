26f33e7eb9d19652ff249730646ef0fa5a145135
NgXCyclone <44649334+NgXCyclone@users.noreply.github.com>
2021-01-28 03:30:42 +0000

    Changed test.js to help.js

--- commands/test.js
+++ commands/test.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js")
+const bot = require("../index.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    message.channel.send("test")
+    message.channel.send({embed: {color: "RANDOM", description: `**<@${bot.user.id}>insultme:** I will insult you\n\n**<@${bot.user.id}>randomme:** I will send a random message I always say to people`}})
 
 }
 
   exports.help = {
-  name: "test",
+  name: "help",
   aliases: []
 }
